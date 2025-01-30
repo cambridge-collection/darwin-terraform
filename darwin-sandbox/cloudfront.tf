225b19d3104f1fbde6847dbb20d7b8242454b358
Michael Hawkins <mjh39@cam.ac.uk>
2025-01-30 14:53:46 +0000

    function needs environment specific name

--- darwin-sandbox/cloudfront.tf
+++ darwin-sandbox/cloudfront.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudfront_function" "darwin" {
-  name    = "clean_urls"
+  name    = "${local.environment}-clean_urls"
   runtime = "cloudfront-js-2.0"
   comment = "clean-and-redirect"
   publish = true
