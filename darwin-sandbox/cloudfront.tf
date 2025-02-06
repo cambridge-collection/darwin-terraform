9bf7af19fbc0f9ecd648d70f8b74d841aa419297
Michael Hawkins <mjh39@cam.ac.uk>
2025-02-06 17:54:02 +0000

    feat: add authentication

--- darwin-sandbox/cloudfront.tf
+++ darwin-sandbox/cloudfront.tf
@@ -3,5 +3,50 @@ resource "aws_cloudfront_function" "darwin" {
   runtime = "cloudfront-js-2.0"
   comment = "clean-and-redirect"
   publish = true
+  key_value_store_associations = [aws_cloudfront_key_value_store.viewer.arn]
   code    = file("${path.module}/templates/darwin/cloudfront-function.js.ttfpl")
+}
+
+resource "aws_cloudfront_function" "search" {
+  name    = "${local.environment}-search-api"
+  runtime = "cloudfront-js-2.0"
+  comment = "search api frontend"
+  publish = true
+  key_value_store_associations = [aws_cloudfront_key_value_store.viewer.arn]
+  code    = file("${path.module}/templates/darwin/search-frontend.js.ttfpl")
+}
+
+resource "aws_cloudfront_key_value_store" "viewer" {
+  name = "${local.environment}-cudl-viewer"
+}
+
+resource "aws_cloudfrontkeyvaluestore_key" "username" {
+  key_value_store_arn = aws_cloudfront_key_value_store.viewer.arn
+  key                 = "username"
+  value               = data.aws_ssm_parameter.cudl_viewer_cloudfront_username.value
+}
+
+resource "aws_cloudfrontkeyvaluestore_key" "password" {
+  key_value_store_arn = aws_cloudfront_key_value_store.viewer.arn
+  key                 = "password"
+  value               = data.aws_ssm_parameter.cudl_viewer_cloudfront_password.value
+}
+
+resource "aws_cloudfrontkeyvaluestore_key" "domain" {
+  key_value_store_arn = aws_cloudfront_key_value_store.viewer.arn
+  key                 = "domain"
+  value               = "cudl-sandbox.net"
+  # The value should be generated from registered_domain_name (with a replace to remove trailing period)
+}
+
+resource "aws_cloudfrontkeyvaluestore_key" "privateSite" {
+  key_value_store_arn = aws_cloudfront_key_value_store.viewer.arn
+  key                 = "privateSite"
+  value               = true
+}
+
+resource "aws_cloudfrontkeyvaluestore_key" "secret" {
+  key_value_store_arn = aws_cloudfront_key_value_store.viewer.arn
+  key                 = "secret"
+  value               = data.aws_ssm_parameter.cudl_viewer_cloudfront_secret.value
 }
